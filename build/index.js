89e34ae2f878fea10d9932d4352a70724025e9c6
Ryuunofire27 <cdvillagomez27@gmail.com>
2018-02-16 12:37:29 -0500

    writing-animation

---
+++ build/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var writingAnimation = function writingAnimation(text, textContainer, delay) {
+  var changeDelay = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 0;
+  var stop = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : true;
+
+  var textArray = text.split('');
+  var i = 0;
+  var write = true;
+  var textAnimate = setInterval(function () {
+    if (write) {
+      if (i < textArray.length) {
+        textContainer.innerHTML += textArray[i] === ' ' ? ' ' + textArray[i += 1] : textArray[i];
+        i += 1;
+      } else {
+        write = !write;
+        if (stop) {
+          clearInterval(textAnimate);
+        } else {
+          setTimeout(function () {
+            i = 0;
+            textContainer.innerHTML = '';
+            write = !write;
+          }, changeDelay);
+        }
+      }
+    }
+  }, delay);
+};
+
+exports.default = writingAnimation;
